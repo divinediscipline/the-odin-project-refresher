b7f7b69faf878aa4481b8531b859c6c188685dd5
Divinelove Chukwuemeka <divinediscipline4real@gmail.com>
2020-08-28 06:00:24 +0100

    add bubble sort solution with Ruby and JavaScript

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -5,5 +5,46 @@ Build a method "bubble_sort" that takes an array and returns a sorted array. It 
 
 
 
-# Solution
+# Solution using ruby
+def bubble_sort(arr)
+  sorted = false
+  while sorted == false do
+    sorted = true
+    arr.each_with_index do |num, i|
+      if i < arr.length - 1
+        if num > arr[i + 1]
+          sorted = false
+          arr[i] = arr[i + 1]
+          arr[i + 1] = num
+        end
+      end
+    end
+  end
+  pp arr
+end
+  
+  bubble_sort([4,3,78,2,0,2])
 
+
+# Solution with JavaScript
+const bubble_sort = (arr) => {
+  sorted = 'False'
+
+  while (sorted == 'False') {
+    sorted = 'True'
+
+    for(let i = 0; i < arr.length - 1; i++){
+      if (i < arr.length - 1) {
+        if (arr[i] > arr[i + 1]) {
+          sorted = 'False'
+          let temp_value = arr[i]
+          arr[i] = arr[i + 1]
+          arr[i + 1] = temp_value // We use a temp value because we won't have access to arr[i] anymore once we modify it in the line above.
+        }
+      }
+    }
+  }
+  return arr
+}
+
+  console.log(bubble_sort([4,3,78,2,0,2]))
